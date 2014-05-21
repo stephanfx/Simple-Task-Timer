ec71cd91054c06234a371ed77fede5cb4f08e6b2
Stephan Grobler <stephangrobler@gmail.com>
2014-05-21 14:17:50 +0200

    added timer

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,74 @@
 angular.module('simpleTask', ['ngRoute'])
-.config(['$routeProvider', function($routeProvider){
+	.config(['$routeProvider',
+		function($routeProvider) {
 
-}])
-.controller('TaskCtrl', ['$scope', function($scope){
-	$scope.tasks = [];
-	$scope.load = function(){
-		if (localStorage.getItem('tasks')){
-			var tasks = JSON.parse(localStorage.getItem('tasks'));
-			console.log(tasks);
-			$scope.tasks = tasks;
 		}
-	};
-	$scope.persist = function(){
-		if (Storage){
-			localStorage.setItem('tasks', JSON.stringify($scope.tasks));
+	])
+	.controller('TaskCtrl', ['$scope', '$timeout',
+		function($scope, $timeout) {
+			$scope.tasks = [];
+			$scope.load = function() {
+				if (localStorage.getItem('tasks')) {
+					var tasks = JSON.parse(localStorage.getItem('tasks'));
+					console.log(tasks);
+					$scope.tasks = tasks;
+					$scope.timing();
+				}
+			};
+			$scope.persist = function() {
+				if (Storage) {
+					localStorage.setItem('tasks', JSON.stringify($scope.tasks));
+				}
+			};
+			$scope.createTask = function() {
+				var start = Date.now();
+				$scope.tasks.unshift({
+					"name": $scope.task.name,
+					"start": start
+				});
+				$scope.persist();
+				$scope.task = null;
+			};
+
+			$scope.removeTask = function(index) {
+				$scope.tasks.splice(index, 1);
+				$scope.persist();
+			};
+
+			$scope.timeLeft = [];
+
+			$scope.timing = function(){
+				$timeout(function(){
+					var now = Date.now();
+					for (var i = $scope.tasks.length - 1; i >= 0; i--) {
+						$scope.timeLeft[i] = now - $scope.tasks[i].start;
+					}
+					$scope.timing();
+				},1000);
+			};
+
+
+			$scope.stopTask = function(index) {
+				var task = $scope.tasks[index];
+				task.end = Date.now();
+				task.total = task.end - task.start;
+				$scope.persist();
+			};
 		}
-	};
-	$scope.createTask = function(){
-		var start = Date.now();
-		$scope.tasks.unshift({"name":$scope.task.name, "start": start});
-		$scope.persist();
-		$scope.task = null;
-	};
+	])
+	.filter('toTime', function() {
+		return function(input) {
+			if (typeof input !== "undefined"){
+				var milliseconds = parseInt((input % 1000) / 100, 10),
+					seconds = parseInt((input / 1000) % 60, 10),
+					minutes = parseInt((input / (1000 * 60)) % 60, 10),
+					hours = parseInt((input / (1000 * 60 * 60)) % 24, 10);
 
-	$scope.removeTask = function(index){
-		$scope.tasks.splice(index,1);
-		$scope.persist();
-	};
+				hours = (hours < 10) ? "0" + hours : hours;
+				minutes = (minutes < 10) ? "0" + minutes : minutes;
+				seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-	$scope.stopTask = function(index){
-		var task = $scope.tasks[index];
-		task.end = Date.now();
-		task.total = task.end - task.start;
-		$scope.persist();
-	};
-}]);
+				return hours + ":" + minutes + ":" + seconds;
+			}
+		};
+	});
