386469c61bdbec00564cb2744eccbfaf5c1b4256
Stephan Grobler <stephangrobler@gmail.com>
2014-07-25 12:29:30 +0200

    Added stt to the github pages to test if it works there

---
+++ stt/static/simpleTask.js
@@ -0,0 +1,308 @@
+/*! simpleTask - v0.0.1 - 2014-07-25
+ * Copyright (c) 2014 ;
+ * Licensed 
+ */
+angular.module('simpleTask', ['ngRoute', 'Reporting', 'templates-main'])
+	.config(['$routeProvider',
+		function($routeProvider) {
+			$routeProvider
+				.when('/', {
+					controller: 'TaskCtrl',
+					templateUrl: 'app/tasks.tpl.html'
+				})
+				.when('/report', {
+					controller: 'ReportingCtrl',
+					templateUrl: 'app/report/report.tpl.html'
+				})
+				.otherwise('/');
+		}
+	])
+	.controller('HeaderCtrl', ['$scope', 'Tasks', '$interval',
+		function($scope, Tasks, $interval) {
+			$scope.task = {};
+			$scope.createTask = function(task) {
+				Tasks.createTask(task);
+				$scope.task = null;
+			};
+			$scope.removeTask = function(index) {
+				Tasks.removeTask(index);
+			};
+			$scope.startBreak = function() {
+				Tasks.createTask({
+					name: "Break",
+					estimate: 0
+				}, 'break');
+				Tasks.startTask(0);
+				Tasks.archiveTask(0);
+				$scope.breakTime = true;
+				$scope.getWorkTimes();
+			};
+			$scope.stopBreak = function() {
+				Tasks.stopTask(0);
+				$scope.breakTime = false;
+				$scope.getWorkTimes();
+			};
+			$scope.getWorkTimes = function() {
+				var times = Tasks.getWorkTimes();
+				$scope.worktimes = times[moment().format('YYYY-MM-DD')];
+			};
+		}
+	])
+	.controller('TaskCtrl', ['$scope', 'Tasks', '$interval',
+		function($scope, Tasks, $interval) {
+			Tasks.load();
+			$scope.tasks = Tasks.tasks;
+			$scope.startTask = function(index) {
+				Tasks.startTask(index);
+				$scope.interval = $interval(function() {
+					Tasks.runningTime(index);
+				}, 1000);
+
+			};
+			$scope.stopTask = function(index) {
+				Tasks.stopTask(index);
+				$interval.cancel($scope.interval);
+
+			};
+			$scope.archiveTask = function(index) {
+				Tasks.archiveTask(index);
+			};
+		}
+	])
+	.filter('toTime', function() {
+		return function(input) {
+			if (typeof input !== "undefined") {
+				var m = moment.duration(input),
+					min = m.get('m'),
+					sec = m.get('s');
+				if (min < 10) {
+					min = "0" + min;
+				}
+				if (sec < 10) {
+					sec = "0" + sec;
+				}
+				return m.get('h') + ":" + min + ":" + sec;
+			}
+		};
+	});
+
+angular.module('Tasks', []).
+factory('Tasks', function() {
+	return {
+		tasks: [],
+		timeLeft: [],
+		load: function() {
+			if (localStorage.getItem('tasks')) {
+				var storedtasks = JSON.parse(localStorage.getItem('tasks'));
+				tasks = this.tasks = storedtasks;
+				for (var i = tasks.length - 1; i >= 0; i--) {
+
+					if (typeof tasks[i + 1] !== 'undefined') {
+						var day = new Date(tasks[i].start).toString('yyyy-MM-dd'),
+							nextDay = new Date(tasks[i + 1].start).toString('yyyy-MM-dd');
+						if (day !== nextDay) {
+							tasks[i].newDay = true;
+						}
+					}
+				}
+			}
+		},
+		persist: function() {
+			if (Storage) {
+				localStorage.setItem('tasks', JSON.stringify(this.tasks));
+			}
+		},
+		fixTimes: function(task) {
+			if (typeof task.times == "undefined") {
+				task.times = [];
+				var time = {
+					start: task.start,
+					end: task.end
+				};
+				task.times.push(time);
+				delete task.start;
+				delete task.end;
+			}
+			if (typeof task.type == "undefined") {
+				task.type = "work";
+			}
+			return task;
+		},
+		createTask: function(task, type) {
+			if (typeof type == "undefined") {
+				type = 'work';
+			}
+			this.tasks.unshift({
+				"name": task.name,
+				"estimate": task.estimate,
+				"type": type,
+				"times": [],
+				"total": 0,
+				"running": false
+			});
+			this.persist();
+		},
+		startTask: function(index) {
+			var start = moment().valueOf();
+			this.tasks[index].times.push({
+				"start": start,
+				"end": null
+			});
+			this.tasks[index].running = true;
+			this.persist();
+		},
+		runningTime: function(index) {
+			var task = this.tasks[index],
+				latest = task.times.length - 1;
+			task.runningTime = (moment().valueOf() - task.times[latest].start) + task.total;
+		},
+		removeTask: function(index) {
+			this.tasks.splice(index, 1);
+			this.persist();
+		},
+
+		archiveTask: function(index) {
+			this.tasks[index].archived = true;
+			this.persist();
+		},
+		stopTask: function(index) {
+			var task = this.tasks[index],
+				timeToStop = task.times.length - 1;
+			task.running = false;
+			task.times[timeToStop].end = moment().valueOf();
+			this.calculateTime(index);
+
+			this.persist();
+		},
+
+		calculateTime: function(index) {
+			var task = this.tasks[index];
+			task.total = 0;
+			for (var x = task.times.length - 1; x >= 0; x--) {
+				task.times[x].total = task.times[x].end - task.times[x].start;
+				task.total += task.times[x].total;
+			}
+		},
+		getWorkTimes: function() {
+			var workHrs = {};
+			for (var x = this.tasks.length - 1; x >= 0; x--) {
+				var task = this.tasks[x];
+				// get days worked on
+				if (typeof task.times == "undefined") {
+					continue;
+				} else {
+					for (var i = task.times.length - 1; i >= 0; i--) {
+						var sdate = moment(task.times[i].start),
+							edate = moment(task.times[i].end),
+							dayFormat = sdate.format('YYYY-MM-DD');
+						if (typeof workHrs[dayFormat] == "undefined") {
+							workHrs[dayFormat] = {
+								"work": 0,
+								"breaktime": 0
+							};
+						}
+						if (task.times[i].end !== null && task.type == "work") {
+							var total = edate.valueOf() - sdate.valueOf();
+							workHrs[dayFormat].work += total;
+						}
+						if (task.times[i].end !== null && task.type == "break") {
+							var breaktotal = edate.valueOf() - sdate.valueOf();
+							workHrs[dayFormat].breaktime += breaktotal;
+						}
+					}
+				}
+
+			}
+			return workHrs;
+		},
+		getDailyTimes: function() {
+			var report = {};
+			//Step through all tasks
+			for (var i = this.tasks.length - 1; i >= 0; i--) {
+				// Step through all times logged for the task
+				var task = this.tasks[i];
+				for (var j = task.times.length - 1; j >= 0; j--) {
+					var time = task.times[j];
+					// determine date and see if it is in the report array
+					var startTime = moment(time.start),
+						endTime = moment(time.end),
+						startTimeStr = startTime.format('YYYY-MM-DD');
+					// add date to report if it is not there yet.
+					if (!report.hasOwnProperty(startTimeStr)) {
+						report[startTimeStr] = {};
+					}
+					// add task if not there yet. with a total of 0
+					if (!report[startTimeStr][task.name]) {
+						report[startTimeStr][task.name] = {
+							total: 0
+						};
+					}
+
+					timespent = time.end - time.start;
+					report[startTimeStr][task.name].total = report[startTimeStr][task.name].total + timespent;
+				}
+			}
+			return report;
+		}
+
+	};
+});
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+/**
+* Reporting Module
+*
+* Reporting module to be used in the app
+*/
+angular.module('Reporting', ['Tasks']).
+controller('ReportingCtrl', ['$scope', 'Tasks', function($scope, Tasks){
+	$scope.results = Tasks.getDailyTimes();
+}]);
---
+++ stt/static/templates.js
@@ -0,0 +1,97 @@
+angular.module('templates-main', ['app/header.tpl.html', 'app/report/report.tpl.html', 'app/tasks.tpl.html']);
+
+angular.module("app/header.tpl.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("app/header.tpl.html",
+    "<div class=\"pull-right\">\n" +
+    "	<a class=\"action\" href ng-hide=\"breakTime\" ng-click=\"startBreak()\"><span class=\"glyphicon glyphicon-cutlery\"></span></a>\n" +
+    "	<a class=\"action\" href ng-hide=\"!breakTime\" ng-class=\"{'running': breakTime}\" ng-click=\"stopBreak()\"><span class=\"glyphicon glyphicon-cutlery\"></span></a>\n" +
+    "</div>\n" +
+    "<div class=\"page-header\">\n" +
+    "\n" +
+    "	<h1><a href ng-click=\"settings = !settings\">		<span class=\"glyphicon glyphicon-cog\"></span></a>Simple Task Timer</h1>\n" +
+    "\n" +
+    "</div>\n" +
+    "<div ng-show=\"settings\">\n" +
+    "	<a href ng-click=\"getWorkTimes()\">GetTimes</a>\n" +
+    "	Worked: {{worktimes.work | toTime}}\n" +
+    "	Breaks: {{worktimes.breaktime | toTime}}\n" +
+    "	<a href=\"#/report\">Report</a>\n" +
+    "	<a href=\"#/\">Tasks</a>\n" +
+    "</div>\n" +
+    "\n" +
+    "<form action method=\"POST\" name=\"taskForm\" class=\"form-inline\" role=\"form\" novalidate>\n" +
+    "	<div class=\"form-group\">\n" +
+    "		<label for=\"inputTaskName\" class=\"sr-only\">Task Name:</label>\n" +
+    "		<div class=\"\">\n" +
+    "			<input type=\"text\" name=\"taskName\" id=\"inputTaskName\" ng-model=\"task.name\" class=\"form-control\" placeholder=\"Task Description\" required=\"required\">\n" +
+    "		</div>\n" +
+    "	</div>\n" +
+    "	<div class=\"form-group\">\n" +
+    "		<label for=\"inputEstimatedTime\" class=\"sr-only\">Estimated Time:</label>\n" +
+    "		<div class=\"\">\n" +
+    "			<input type=\"text\" name=\"EstimatedTime\" id=\"inputEstimatedTime\" ng-model=\"task.estimate\" class=\"form-control\" placeholder=\"Estimated time\" required=\"required\">\n" +
+    "		</div>\n" +
+    "	</div>\n" +
+    "\n" +
+    "	<div class=\"form-group\">\n" +
+    "		<div class=\"\">\n" +
+    "			<button ng-click=\"createTask(task)\" ng-disabled=\"!taskForm.$valid\" class=\"btn btn-primary\">Submit</button>\n" +
+    "		</div>\n" +
+    "	</div>\n" +
+    "</form>");
+}]);
+
+angular.module("app/report/report.tpl.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("app/report/report.tpl.html",
+    "<h1>Reporting</h1>\n" +
+    "<ul class=\"list-unstyled\">\n" +
+    "	<li ng-repeat=\"(key, result) in results | orderBy:-key\">\n" +
+    "		<div class=\"page-header\">\n" +
+    "		  <h3>{{key}}</h3>\n" +
+    "		</div>\n" +
+    "		<ul class=\"list-unstyled\">\n" +
+    "			<li ng-repeat=\"(task, time) in result\">\n" +
+    "				 {{time.total | toTime}} - {{task}}\n" +
+    "			</li>\n" +
+    "		</ul>\n" +
+    "	</li>\n" +
+    "</ul>");
+}]);
+
+angular.module("app/tasks.tpl.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("app/tasks.tpl.html",
+    "<ul class=\"list-unstyled\">\n" +
+    "	<li ng-repeat=\"task in tasks track by $index\" ng-hide=\"task.archived\" ng-class=\"{'day-seperator': task.newDay, 'running': task.running}\" >\n" +
+    "		<div class=\"row\">\n" +
+    "			<div class=\"col-xs-12 col-sm-8\">\n" +
+    "				<div style=\"padding: 4px 10px\">\n" +
+    "					<div>\n" +
+    "						<h3>{{task.name}}&nbsp;\n" +
+    "							<span class=\"small\" ng-hide=\"task.running\">{{task.total | toTime}} </span>\n" +
+    "							<span class=\"small\" ng-show=\"task.running\">{{ task.runningTime | toTime}}</span>\n" +
+    "						</h3>\n" +
+    "					</div>\n" +
+    "					<div><a href ng-click=\"displayTimes = !displayTimes\">{{task.times.length}} time entries</a></div>\n" +
+    "					<div>{{task.estimate}} hrs estimated {{task.type}}</div>\n" +
+    "\n" +
+    "				</div>\n" +
+    "			</div>\n" +
+    "			<div class=\"col-xs-12 col-sm-4 text-center\">\n" +
+    "				<a class=\"action\" ng-show=\"!task.running\" href ng-click=\"startTask($index)\"><span class=\"glyphicon glyphicon-play\"></span></a>\n" +
+    "				<a class=\"action\" ng-show=\"task.running\" href ng-click=\"stopTask($index)\"><span class=\"glyphicon glyphicon-stop\"></span></a>\n" +
+    "				<a class=\"action\" href ng-click=\"archiveTask($index)\"><span class=\"glyphicon glyphicon-ok\"></span></a>\n" +
+    "			</div>\n" +
+    "		</div>\n" +
+    "		<div class=\"row\" ng-show=\"displayTimes\">\n" +
+    "			<div class=\"col-xs-12\">\n" +
+    "				<ul class=\"list-unstyled\">\n" +
+    "					<li ng-repeat=\"time in task.times track by $index\">\n" +
+    "						Time Started: {{time.start | date:\"yyyy-MM-dd HH:mm:ss\"}} |\n" +
+    "						Time Ended: {{time.end | date:\"yyyy-MM-dd HH:mm:ss\"}} <br>\n" +
+    "					</li>\n" +
+    "				</ul>\n" +
+    "			</div>\n" +
+    "		</div>\n" +
+    "	</li>\n" +
+    "</ul>");
+}]);
