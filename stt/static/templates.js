46a2ce4e2fa4d65d060beb7c2d5b85d7fe9be0ac
Stephan Grobler <stephangrobler@gmail.com>
2014-12-02 16:14:35 +0200

    Updates to the UI

--- stt/static/templates.js
+++ stt/static/templates.js
@@ -2,24 +2,43 @@ angular.module('templates-main', ['app/header.tpl.html', 'app/report/report.tpl.
 
 angular.module("app/header.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("app/header.tpl.html",
-    "<div class=\"pull-right\">\n" +
-    "	<a class=\"action\" href ng-hide=\"breakTime\" ng-click=\"startBreak()\"><span class=\"glyphicon glyphicon-cutlery\"></span></a>\n" +
-    "	<a class=\"action\" href ng-hide=\"!breakTime\" ng-class=\"{'running': breakTime}\" ng-click=\"stopBreak()\"><span class=\"glyphicon glyphicon-cutlery\"></span></a>\n" +
-    "</div>\n" +
-    "<div class=\"page-header\">\n" +
+    "<div>\n" +
+    "	<h1 class=\"text-center\">\n" +
     "\n" +
-    "	<h1><a href ng-click=\"settings = !settings\">		<span class=\"glyphicon glyphicon-cog\"></span></a>Simple Task Timer</h1>\n" +
+    "		Simple Task Timer\n" +
+    "	</h1>\n" +
     "\n" +
     "</div>\n" +
-    "<div ng-show=\"settings\">\n" +
-    "	<a href ng-click=\"getWorkTimes()\">GetTimes</a>\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col-xs-12\">\n" +
+    "		<div class=\"action-buttons text-center\">\n" +
+    "			<ul>\n" +
+    "				<li>\n" +
+    "					<a href class=\"action no-margin\" ng-click=\"settings = !settings\"><i class=\"fa fa-bars\"></i></a>\n" +
+    "				</li>\n" +
+    "				<li>\n" +
+    "					<a href class=\"action no-margin\" ng-click=\"addTask = !addTask\"><i class=\"fa fa-plus\"></i></a>\n" +
+    "				</li>\n" +
+    "				<li>\n" +
+    "					<a class=\"action no-margin\" href ng-hide=\"breakTime\" ng-click=\"startBreak()\"><i class=\"fa fa-coffee\"></i></a>\n" +
+    "					<a class=\"action no-margin\" href ng-hide=\"!breakTime\" ng-class=\"{'running': breakTime}\" ng-click=\"stopBreak()\"><i class=\"fa fa-coffee\"></i></a>\n" +
+    "				</li>\n" +
+    "			</ul>\n" +
+    "		</div>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "\n" +
+    "<div ng-show=\"settings\" class=\"action-slide\">\n" +
+    "	<ul class=\"nav nav-pills\">\n" +
+    "		<li><a href=\"#/\">Tasks</a></li>\n" +
+    "		<li><a href ng-click=\"getWorkTimes()\">GetTimes</a></li>\n" +
+    "		<li><a href=\"#/report\">Report</a></li>\n" +
+    "	</ul>\n" +
     "	Worked: {{worktimes.work | toTime}}\n" +
     "	Breaks: {{worktimes.breaktime | toTime}}\n" +
-    "	<a href=\"#/report\">Report</a>\n" +
-    "	<a href=\"#/\">Tasks</a>\n" +
     "</div>\n" +
     "\n" +
-    "<form action method=\"POST\" name=\"taskForm\" class=\"form-inline\" role=\"form\" novalidate>\n" +
+    "<form ng-show=\"addTask\" action method=\"POST\" name=\"taskForm\" class=\"form-inline action-slide\" role=\"form\" novalidate>\n" +
     "	<div class=\"form-group\">\n" +
     "		<label for=\"inputTaskName\" class=\"sr-only\">Task Name:</label>\n" +
     "		<div class=\"\">\n" +
@@ -38,7 +57,8 @@ angular.module("app/header.tpl.html", []).run(["$templateCache", function($templ
     "			<button ng-click=\"createTask(task)\" ng-disabled=\"!taskForm.$valid\" class=\"btn btn-primary\">Submit</button>\n" +
     "		</div>\n" +
     "	</div>\n" +
-    "</form>");
+    "</form>\n" +
+    "");
 }]);
 
 angular.module("app/report/report.tpl.html", []).run(["$templateCache", function($templateCache) {
@@ -55,43 +75,56 @@ angular.module("app/report/report.tpl.html", []).run(["$templateCache", function
     "			</li>\n" +
     "		</ul>\n" +
     "	</li>\n" +
+    "	<li style=\"margin-bottom: 20px;\"></li>\n" +
     "</ul>");
 }]);
 
 angular.module("app/tasks.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("app/tasks.tpl.html",
-    "<ul class=\"list-unstyled\">\n" +
-    "	<li ng-repeat=\"task in tasks track by $index\" ng-hide=\"task.archived\" ng-class=\"{'day-seperator': task.newDay, 'running': task.running}\" >\n" +
-    "		<div class=\"row\">\n" +
-    "			<div class=\"col-xs-12 col-sm-8\">\n" +
-    "				<div style=\"padding: 4px 10px\">\n" +
-    "					<div>\n" +
-    "						<h3>{{task.name}}&nbsp;\n" +
-    "							<span class=\"small\" ng-hide=\"task.running\">{{task.total | toTime}} </span>\n" +
-    "							<span class=\"small\" ng-show=\"task.running\">{{ task.runningTime | toTime}}</span>\n" +
-    "						</h3>\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col-xs-12\">\n" +
+    "		<ul class=\"list-unstyled\">\n" +
+    "			<li class=\"task\" ng-repeat=\"task in tasks track by $index\" ng-hide=\"task.archived\" ng-class=\"{'day-seperator': task.newDay, 'running': task.running}\" >\n" +
+    "				<div class=\"row\">\n" +
+    "					<div class=\"col-xs-12 col-sm-8\">\n" +
+    "						<div class=\"task-description\">\n" +
+    "							<div>\n" +
+    "								<h3>{{task.name}}&nbsp;\n" +
+    "									<span class=\"small\" ng-hide=\"task.running\">{{task.total | toTime}} </span>\n" +
+    "									<span class=\"small\" ng-show=\"task.running\">{{ task.runningTime | toTime}}</span>\n" +
+    "								</h3>\n" +
+    "							</div>\n" +
+    "							<div><a href ng-click=\"displayTimes = !displayTimes\">{{task.times.length}} time entries</a></div>\n" +
+    "							<div>{{task.estimate}} hrs estimated {{task.type}}</div>\n" +
+    "						</div>\n" +
     "					</div>\n" +
-    "					<div><a href ng-click=\"displayTimes = !displayTimes\">{{task.times.length}} time entries</a></div>\n" +
-    "					<div>{{task.estimate}} hrs estimated {{task.type}}</div>\n" +
-    "\n" +
+    "					<div class=\"col-xs-12 col-sm-4\">\n" +
+    "						<div class=\"action-buttons\">\n" +
+    "							<ul>\n" +
+    "								<li>\n" +
+    "									<a class=\"action\" ng-show=\"!task.running\" href ng-click=\"startTask($index)\"><i class=\"fa fa-play\"></i></a>\n" +
+    "									<a class=\"action\" ng-show=\"task.running\" href ng-click=\"stopTask($index)\"><i class=\"fa fa-stop\"></i></a>\n" +
+    "								</li>\n" +
+    "								<li>\n" +
+    "									<a class=\"action\" href ng-click=\"archiveTask($index)\"><i class=\"fa fa-check\"></i></a>\n" +
+    "								</li>\n" +
+    "							</ul>\n" +
+    "						</div>\n" +
+    "					</div>\n" +
     "				</div>\n" +
-    "			</div>\n" +
-    "			<div class=\"col-xs-12 col-sm-4 text-center\">\n" +
-    "				<a class=\"action\" ng-show=\"!task.running\" href ng-click=\"startTask($index)\"><span class=\"glyphicon glyphicon-play\"></span></a>\n" +
-    "				<a class=\"action\" ng-show=\"task.running\" href ng-click=\"stopTask($index)\"><span class=\"glyphicon glyphicon-stop\"></span></a>\n" +
-    "				<a class=\"action\" href ng-click=\"archiveTask($index)\"><span class=\"glyphicon glyphicon-ok\"></span></a>\n" +
-    "			</div>\n" +
-    "		</div>\n" +
-    "		<div class=\"row\" ng-show=\"displayTimes\">\n" +
-    "			<div class=\"col-xs-12\">\n" +
-    "				<ul class=\"list-unstyled\">\n" +
-    "					<li ng-repeat=\"time in task.times track by $index\">\n" +
-    "						Time Started: {{time.start | date:\"yyyy-MM-dd HH:mm:ss\"}} |\n" +
-    "						Time Ended: {{time.end | date:\"yyyy-MM-dd HH:mm:ss\"}} <br>\n" +
-    "					</li>\n" +
-    "				</ul>\n" +
-    "			</div>\n" +
-    "		</div>\n" +
-    "	</li>\n" +
-    "</ul>");
+    "				<div class=\"row\" ng-show=\"displayTimes\">\n" +
+    "					<div class=\"col-xs-12\">\n" +
+    "						<ul class=\"list-unstyled\">\n" +
+    "							<li ng-repeat=\"time in task.times track by $index\">\n" +
+    "								Time Started: {{time.start | date:\"yyyy-MM-dd HH:mm:ss\"}} |\n" +
+    "								Time Ended: {{time.end | date:\"yyyy-MM-dd HH:mm:ss\"}} <br>\n" +
+    "							</li>\n" +
+    "						</ul>\n" +
+    "					</div>\n" +
+    "				</div>\n" +
+    "			</li>\n" +
+    "		</ul>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "");
 }]);
